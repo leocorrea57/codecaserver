093afa1143079568e7914e12aa65f6d0c369a785
vinigodoy <vinigodoy@gmail.com>
2023-06-17 09:39:53 -0300

    Unit tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,10 @@ dependencies {
     runtimeOnly("com.h2database:h2")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.mockk:mockk:1.13.5")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.6.2")
+
+    runtimeOnly("io.kotest:kotest-assertions-core:5.6.2")
 }
 
 tasks.withType<KotlinCompile> {
